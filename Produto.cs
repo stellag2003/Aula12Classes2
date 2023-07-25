65baa22c48fac40921bee09727ec94eca14d4f33
Stella Gonçalves <135771572+stellag2003@users.noreply.github.com>
2023-07-25 17:17:48 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes2
+{
+    
+    internal class Produto
+    {
+        public string nome;
+        public string descricao;
+        public int preco;
+        public int quantidade;
+        public bool status = true;
+
+        
+
+        public void mostra()
+        {
+            Console.WriteLine($"O produto é {nome}, descrição: {descricao}, preço {preco}, no estoque tem  {quantidade}, o status é  {status}. ");
+        }
+        
+        public void desativa()
+        {
+            Console.WriteLine($"Quantidade {quantidade = 0}, status {status = false} ");
+        }
+       
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Aula12Classes2;
+
+Produto produto1= new Produto();
+
+produto1.nome = "batom";
+produto1.quantidade= 34;
+produto1.descricao = "produto de beleza";
+produto1.preco = 25;
+produto1.status = true;
+
+produto1.mostra();
+produto1.desativa();
+
+
+Produto produto2 = new Produto();
+
+produto2.nome = "gloss";
+produto2.quantidade = 10;
+produto2.descricao = "produto de beleza";
+produto2.preco = 15;
+produto2.status = true;
+
+produto2.mostra();
+produto2.desativa();
+produto2.mostra();
+
+Produto produto3= new Produto();
+
+produto3.nome = "lápis de olho";
+produto3.quantidade = 27;
+produto3.descricao = "produto de beleza";
+produto3.preco = 20;
+produto3.status = true;
+
+produto3.mostra();
+Console.ReadLine();
+
+
